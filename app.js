5a4e90d85ce90b12935982b1131f5fc4e9aa7fdb
Darren Ross <darren.ross@bellevuecollege.edu>
2020-02-26 20:35:33 -0800

    Updated.

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/themes/Prog209ThemeRolled.min.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/themes/Prog209ThemeRolled.min.css'));
+});
+
+app.get('/themes/jquery.mobile.icons.min.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/themes/jquery.mobile.icons.min.css'));
+});
+
 module.exports = app;
